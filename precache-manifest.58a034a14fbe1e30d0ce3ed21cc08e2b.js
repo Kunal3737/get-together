1fd5cb7946611abe022c22bb6ae4010a156df3f2
Kunal Shinde <kunalshinde373@gmail.com>
2020-04-26 08:50:28 +0530

    Updates

---
+++ precache-manifest.58a034a14fbe1e30d0ce3ed21cc08e2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "281c93b503ff888313e11c581a7796e7",
+    "url": "/get-together/index.html"
+  },
+  {
+    "revision": "c318167be36c7721ddd3",
+    "url": "/get-together/static/css/main.51927aa7.chunk.css"
+  },
+  {
+    "revision": "5e9534ae9b3ce420e130",
+    "url": "/get-together/static/js/2.b97003ed.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/get-together/static/js/2.b97003ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c318167be36c7721ddd3",
+    "url": "/get-together/static/js/main.ea052854.chunk.js"
+  },
+  {
+    "revision": "bb54d71c6c72651d7718",
+    "url": "/get-together/static/js/runtime-main.f5aeeb2c.js"
+  }
+]);
